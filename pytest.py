49729fdd4cf3e76c71c4102575a6b2d4f4cff3df
caceres <dfcaceresmoren@yahoo.es>
2018-03-12 13:38:33 +0000

    travis

--- pytest.py
+++ pytest.py
@@ -1,23 +1,53 @@
 import FreeCAD
-FreeCAD.openDocument('/home/travis/SlopedPlanesTest/hello_world.fcstd')
-doc = FreeCAD.ActiveDocument
-print '###### ', doc.Name
-for obj in doc.Objects:
-    if hasattr(obj, 'Proxy'):
-        if obj.Proxy.Type == 'SlopedPlanes':
-            oldShape = obj.Shape.copy()
-            obj.touch()
-            print '### ', obj.Name
-            doc.recompute()
-            newShape = obj.Shape
-            cut = oldShape.copy().cut(newShape)
-            cc = newShape.copy().cut(oldShape)
-            if cut.Area != 0 or\
-               cc.Area != 0 or\
-               len(newShape.Edges) != len(oldShape.Edges) or\
-               len(newShape.Vertexes) != len(oldShape.Vertexes):
-                print '????????????????????????? geometric ERROR'
-            elif obj.State[0] == 'Invalid':
-                print '????????????????????????? execution ERROR'
-            else:
-                print '# okey'
+import os
+
+for directory in os.walk('/home/travis/SlopedPlanesTest'):
+    for filename in directory[1]:
+        if filename.endswith('.fcstd'):
+            FreeCAD.openDocument('/home/travis/SlopedPlanesTest/' +
+                                 directory[0] + filename)
+
+numDoc = 0
+numObj = 0
+numError = 0
+
+for doc in FreeCAD.listDocuments().values():
+    numDoc += 1
+    print '######### ', doc.Name
+    for obj in doc.Objects:
+        if hasattr(obj, 'Proxy'):
+            if obj.Proxy.Type == 'SlopedPlanes':
+                numObj += 1
+                oldShape = obj.Shape.copy()
+
+                '''obj.Proxy.faceList = []
+                for pyFace in obj.Proxy.Pyth:
+                    pyFace.reset = True'''
+
+                obj.touch()
+                print '####### ', obj.Name
+                doc.recompute()
+                newShape = obj.Shape
+                cut = oldShape.copy().cut(newShape)
+                cc = newShape.copy().cut(oldShape)
+
+                if cut.Area != 0 or\
+                   cc.Area != 0 or\
+                   len(newShape.Edges) != len(oldShape.Edges) or\
+                   len(newShape.Vertexes) != len(oldShape.Vertexes):
+
+                    numError += 1
+                    print '????????????????????????? geometric ERROR'
+
+                elif obj.State[0] == 'Invalid':
+
+                    numError += 1
+                    print '????????????????????????? execution ERROR'
+
+                else:
+
+                    print '# okey'
+
+print 'files ', numDoc
+print 'objects ', numObj
+print 'erros ', numError
