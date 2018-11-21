084b94affca5b3de78dad3fa00c919ecd9f9cfe7
caceres <dfcaceresmoren@yahoo.es>
2018-11-21 20:53:03 +0000

    alignment rango

--- SlopedPlanesPyAlignment.py
+++ SlopedPlanesPyAlignment.py
@@ -1089,7 +1089,7 @@ class _PyAlignment(_Py):
 
             # simulating
 
-            if falsify:
+            '''if falsify:
 
                 pyOne.simulating([enormousBase])
                 pyTwo.simulating([enormousCont])
@@ -1097,22 +1097,22 @@ class _PyAlignment(_Py):
             else:
 
                 pyOne.simulating([enormousBase])
-                pyTwo.simulating([enormousBase])
+                pyTwo.simulating([enormousBase])'''
 
             if pyOne.virtualized:
                 pyO = self.selectBasePlane(pyOne.numWire, pyOne.numGeom)
                 # print('1')
-                pyO.simulating([enormousBase])
+                #pyO.simulating([enormousBase])
 
             if pyTwo.virtualized:
                 pyT = self.selectBasePlane(pyTwo.numWire, pyTwo.numGeom)
                 # print('2')
-                if falsify:
+                '''if falsify:
                     # print('21')
                     pyT.simulating([enormousCont])
                 else:
                     # print('22')
-                    pyT.simulating([enormousBase])
+                    pyT.simulating([enormousBase])'''
 
             rChopPy = rangoChopPy[numChop]
             # print('rChop ', self.rango, rChopPy)
