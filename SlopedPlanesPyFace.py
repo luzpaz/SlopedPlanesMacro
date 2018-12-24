055b6ea2a7f6c4d64e3b13ae0f81dd67c470deee
caceres <dfcaceresmoren@yahoo.es>
2018-12-24 17:31:04 +0000

    url

--- SlopedPlanesPyFace.py
+++ SlopedPlanesPyFace.py
@@ -36,7 +36,7 @@ V = FreeCAD.Vector
 
 __title__ = "SlopedPlanes Macro"
 __author__ = "Damian Caceres Moreno"
-__url__ = "http://www.freecadweb.org"
+__url__ = "https://gitlab.com/damianCaceres/slopedplanes"
 __version__ = ""
 
 
