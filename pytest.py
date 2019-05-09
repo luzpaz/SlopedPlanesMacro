f03dcc5078e66d602724ea30f7386fa86e08b93a
damianCaceres <damiancaceresmoreno@yahoo.es>
2019-05-09 11:22:43 +0100

    test problem

--- pytest.py
+++ pytest.py
@@ -13,13 +13,9 @@ executionList = []
 
 
 for directory in os.walk('/home/slopedplanestest/Test'):
-    # print('directory ', directory)
     for filename in directory[2]:
-        #print('filename ', filename)
         if filename.endswith('.fcstd'):
-            # print('open')
             doc = FreeCAD.openDocument(directory[0] + '/' + filename)
-            #print('######### ', doc.Name)
             numDoc += 1
 
             for obj in doc.Objects:
@@ -34,7 +30,6 @@ for directory in os.walk('/home/slopedplanestest/Test'):
                             pyFace.reset = True
 
                         obj.touch()
-                        # print('####### ', obj.Name)
                         doc.recompute()
 
                         newShape = obj.Shape
@@ -46,44 +41,36 @@ for directory in os.walk('/home/slopedplanestest/Test'):
 
                             numError += 1
                             executionList.append((doc.Name, obj.Name))
-                            # print('????????????????????????? execution ERROR')
 
                         elif cut.isNull() or cc.isNull():
 
                             numError += 1
                             geometricList.append((doc.Name, obj.Name))
-                            # print('????????????????????????? geometric ERROR')
 
                         elif cut.Area != 0 or cc.Area != 0:
 
                             numError += 1
                             geometricList.append((doc.Name, obj.Name))
-                            # print('????????????????????????? geometric ERROR')
 
                         elif len(newShape.Edges) != len(oldShape.Edges):
 
                             numError += 1
                             edgeList.append((doc.Name, obj.Name))
-                            # print('????????????????????????? edge ERROR')
 
                         elif len(newShape.Vertexes) != len(oldShape.Vertexes):
 
                             numError += 1
                             vertexList.append((doc.Name, obj.Name))
-                            # print('????????????????????????? vertex ERROR')
 
                         else:
 
                             pass
-                            # print('### okey')
 
-FreeCAD.Console.PrintMessage('files ' + str(numDoc))
-
-print('files ', numDoc)
-print('objects ', numObj)
-print('erros ', numError)
-print('geometry errors ', geometricList)
-print('edge errors ', edgeList)
-print('vertex errors ', vertexList)
-print('execution errors ', executionList)
-print('time.clock() ', time.clock())
+FreeCAD.Console.PrintMessage('files ' + str(numDoc) + '\n')
+FreeCAD.Console.PrintMessage('objects ' + str(numObj) + '\n')
+FreeCAD.Console.PrintMessage('errors ' + str(numError) + '\n')
+FreeCAD.Console.PrintMessage('geometryErrors ' + str(geometricList) + '\n')
+FreeCAD.Console.PrintMessage('edgeErrors ' + str(edgeList) + '\n')
+FreeCAD.Console.PrintMessage('vertexErrors ' + str(vertexList) + '\n')
+FreeCAD.Console.PrintMessage('executionErrors ' + str(executionList) + '\n')
+FreeCAD.Console.PrintMessage('time.clock() ' + str(time.clock()) + '\n')
