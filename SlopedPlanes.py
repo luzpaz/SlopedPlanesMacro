97e1c6e844b4fb0e5296c1e0aad2ff905a0a084d
damianCaceres <damiancaceresmoreno@yahoo.es>
2019-06-30 13:23:40 +0100

    fixes identation

--- SlopedPlanes.py
+++ SlopedPlanes.py
@@ -326,9 +326,9 @@ class _SlopedPlanes(_Py):
                     value = value * cos(radians(ang))
                     # print(ang, height, value)
 
-                    bigFace =\
-                        face.makeOffset2D(offset=value, join=2, fill=False,
-                                          openResult=False, intersection=True)
+                bigFace =\
+                    face.makeOffset2D(offset=value, join=2, fill=False,
+                                      openResult=False, intersection=True)
 
                 coordOutOrd, geomOutOrd, fList =\
                     self.gatherExteriorWires(bigFace.Faces)
